495226e5fc3568ecbe602ef3824f9206f0f3a4b6
Rita D'Aprano <rita.daprano@gmail.com>
2015-03-05 16:26:04 +0100

    Adds fixture adapter

--- js/app.js
+++ js/app.js
@@ -5,18 +5,29 @@ var App = Ember.Application.create({
 App.Router.map(function() {
   this.route('about', { path: '/aboutus'} );
   this.resource('products', function() {
-    this.resource('product', {path: '/:title'});
+    this.resource('product', {path: '/:product_id'});
   });
 });
 
-App.PRODUCTS = [
+App.Product = DS.Model.extend({
+  title: DS.attr(),
+  price: DS.attr(),
+  description: DS.attr(),
+  isOnSale: DS.attr()
+});
+
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+App.Product.FIXTURES = [
   {
+    id: 1,
     title: 'Flint',
     price: 99,
     description: 'Flint is a ..',
     isOnSale: true
   },
   {
+    id: 2,
     title: 'Kind',
     price: 199,
     description: 'Kind is a ..',
@@ -26,13 +37,13 @@ App.PRODUCTS = [
 
 App.ProductsRoute = Ember.Route.extend({
   model: function() {
-    return App.PRODUCTS;
+    return this.store.findAll('product');
   }
 });
 
 App.ProductRoute = Ember.Route.extend({
   model: function(params) {
-    return App.PRODUCTS.findBy('title', params.title);
+    return this.store.find('product', params.product_id);
   }
 });
 
