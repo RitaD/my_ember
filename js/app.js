b6136b2e2696864b80e7efd5b4782b21f34b0df3
Rita D'Aprano <rita.daprano@gmail.com>
2015-03-05 15:34:39 +0100

    Adds product list

--- js/app.js
+++ js/app.js
@@ -7,6 +7,27 @@ App.Router.map(function() {
   this.resource('products');
 });
 
+App.PRODUCTS = [
+  {
+    title: 'Flint',
+    price: 99,
+    description: 'Flint is a ..',
+    isOnSale: true
+  },
+  {
+    title: 'Kind',
+    price: 199,
+    description: 'Kind is a ..',
+    isOnSale: true
+  }
+];
+
+App.ProductsRoute = Ember.Route.extend({
+  model: function() {
+    return App.PRODUCTS;
+  }
+});
+
 App.IndexController = Ember.Controller.extend({
   time: function() {
     return (new Date()).toDateString()
